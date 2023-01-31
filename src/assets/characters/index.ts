51e2960e9f0abe2a41eb82f5244a92247b496999
Sebastian <rockmadero@gmail.com>
2023-01-31 12:33:30 -0500

    characters index

---
+++ src/assets/characters/index.ts
@@ -0,0 +1,11 @@
+import cerberus from './cerberus.jpg';
+import dusa from './dusa_character.jpg';
+import hades from './hades_character.png';
+import megara from './megara_character.png';
+import melinoe from './melinoe_character.jpg';
+import skelly from './skelly.png';
+import thanatos from './thanatos_character.png';
+import zagreus from './zagreus_character.png';
+
+export { cerberus, dusa, hades, megara, melinoe, skelly, thanatos, zagreus };
+
