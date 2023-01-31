c305104b592a57319c7fe7b69b1dd1c94b3ac2a8
Sebastian <rockmadero@gmail.com>
2023-01-31 12:24:27 -0500

    general images index

---
+++ src/assets/index.ts
@@ -0,0 +1,16 @@
+import epicLogo from './epic-logo.png';
+import backImg from './back-img-projectiles.png';
+import github from './github.svg';
+import goty from './goty.png';
+import heroImg from './hades-hero-img.png';
+import hadesLogo from './hades-logo.png';
+import ignLogo from './ign-logo.png';
+import linkdin from './linkdin.svg';
+import playLogo from './playstation-logo.png';
+import skull from './skull_hade.png';
+import steam from './steam-logo.png';
+import switchLogo from './switch-logo.png'
+import twitter from './twitter.svg';
+import xbox from './xbox-logo.png';
+
+export {epicLogo, backImg, github, goty, heroImg, hadesLogo, ignLogo, linkdin, playLogo, skull, steam, twitter, switchLogo, xbox};
