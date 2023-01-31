78ea8be3065828703faf89f350562909e6ddac13
Sebastian <rockmadero@gmail.com>
2023-01-31 12:33:10 -0500

    mirro index

---
+++ src/assets/mirror/index.ts
@@ -0,0 +1,6 @@
+import img1 from './image-mirror-1.jpg';
+import img2 from './image-mirror-2.png';
+import img3 from './image-mirror-3.jpg';
+import img4 from './image-mirror-4.png';
+
+export { img1, img2, img3, img4 };
