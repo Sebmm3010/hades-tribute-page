b38e3b27258231056dda11c465dc30479e750291
Sebastian <rockmadero@gmail.com>
2023-02-02 14:51:53 -0500

    errores menores

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,12 +5,12 @@ import goty from './goty.png';
 import heroImg from './hades-hero-img.png';
 import hadesLogo from './hades-logo.png';
 import ignLogo from './ign-logo.png';
-import linkdin from './linkdin.svg';
+import linkedin from './linkedin.svg';
 import playLogo from './playstation-logo.png';
-import skull from './skull_hade.png';
+import skull from './skull_hades.png';
 import steam from './steam-logo.png';
 import switchLogo from './switch-logo.png'
 import twitter from './twitter.svg';
 import xbox from './xbox-logo.png';
 
-export {epicLogo, backImg, github, goty, heroImg, hadesLogo, ignLogo, linkdin, playLogo, skull, steam, twitter, switchLogo, xbox};
+export { epicLogo, backImg, github, goty, heroImg, hadesLogo, ignLogo, linkedin, playLogo, skull, steam, twitter, switchLogo, xbox};
