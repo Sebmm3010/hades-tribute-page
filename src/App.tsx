8f86affd7f8a48aeb5b75630c6381e8ac9a9a4ba
Sebastian <rockmadero@gmail.com>
2023-02-02 14:53:57 -0500

    stars

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css';
+import './styles/stars.css';
+import { Hero } from './sections';
 
 function App() {
 
   return (
-    <main className='App'>
-      <h1 style={{color:'white'}}>Hola mundo</h1>
-    </main>
+    <>
+      <div id="stars"></div>
+      <div id="star1"></div>
+      <div id="star2"></div>
+      <div id="star3"></div>
+      <main className='App'>
+        <Hero />
+      </main>
+    </>
   )
 }
 
