567e7cf850221950d6562f57f87d9aec89b80cd1
Sebastian <rockmadero@gmail.com>
2023-02-02 20:20:47 -0500

    hero text

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,15 +4,31 @@ import { Navbar } from '../components/Navbar';
 export const Hero = () => {
   return (
     <section className="hero">
-      
+
       <div className="hero_left">
         <Navbar />
+        {/* Hero text */}
+        <div className="hero_text">
+          <div>
+            <span className='outline'>protagoniza </span>
+            <span>una salvaje</span>
+          </div>
+          <div>
+            <span>
+              fuga del </span>
+            <span className='outline'>
+              inframundo
+            </span>
+          </div>
+
+          <div>
+            <span>
+
+            </span>
+          </div>
+        </div>
       </div>
 
-      {/* Hero text */}
-      <div className="hero_text">
-
-      </div>
       <div className="hero_right">Derecha</div>
     </section>
   )
