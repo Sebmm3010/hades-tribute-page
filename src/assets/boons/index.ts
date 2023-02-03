763cbebc6a7ef0236ef9d4b6e3402edca9ed1e04
Sebastian <rockmadero@gmail.com>
2023-02-03 17:32:29 -0500

    boons added

---
+++ src/assets/boons/index.ts
@@ -0,0 +1,12 @@
+import aphrodita from './aphrodite.png';
+import ares from './ares.png';
+import artemisa from './artemis.png';
+import atenea from './atenea.png';
+import chaos from './chaos.png';
+import demeter from './demeter.png';
+import dionisio from './dionisio.png';
+import hermes from './hermes.png';
+import poseidon from './poseidon.png';
+import zeus from './zeus.png';
+
+export {aphrodita, ares, artemisa, atenea, chaos, demeter, dionisio, hermes, poseidon, zeus};
