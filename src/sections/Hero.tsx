b9e93d0743daceefa6ea5231e362e0b2591ff875
Sebastian <rockmadero@gmail.com>
2023-02-03 17:32:49 -0500

    finish hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,5 +1,7 @@
 import '../styles/hero.css';
 import { Navbar } from '../components/Navbar';
+import { boonsData } from '../data';
+import { goty, heroImg, backImg } from '../assets';
 
 export const Hero = () => {
   return (
@@ -7,15 +9,22 @@ export const Hero = () => {
 
       <div className="hero_left">
         <Navbar />
+        {/* Boons */}
+
+        <div className="boons_container">
+          {boonsData.map(item => (
+            <img key={item.alt} src={item.img} alt={item.alt} />
+          ))}
+        </div>
         {/* Hero text */}
+
         <div className="hero_text">
           <div>
-            <span className='outline'>protagoniza </span>
+            <span>protagoniza </span>
             <span>una salvaje</span>
           </div>
           <div>
-            <span>
-              fuga del </span>
+            <span>fuga del </span>
             <span className='outline'>
               inframundo
             </span>
@@ -23,13 +32,43 @@ export const Hero = () => {
 
           <div>
             <span>
-
+              Desafía al mismísimo Dios de los muertos, derrota enemigos y atraviesa mazmorras que te guiaran a un secreto inesperado.
             </span>
+          </div>
+        </div>
+        {/* Metrics */}
+        <div className="metrics">
+          <div>
+            <span>9.3/10</span>
+            <span>En Metacritics</span>
+          </div>
+          <div>
+            <span>1.000.000</span>
+            <span>Copias vendidas</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>Premios ganados</span>
           </div>
         </div>
       </div>
 
-      <div className="hero_right">Derecha</div>
+      <div className="hero_right">
+        <button className='btn'>
+          <a href="https://www.supergiantgames.com/games/hades/" target="_blank" rel="noopener noreferrer">
+          Visitar sitio original
+          </a>
+        </button>
+
+        <div className="goty">
+          <img src={goty} alt="goty" />
+        </div>
+
+        {/* Hero imgs */}
+
+        <img src={heroImg} alt="zagreus" className='hero_img'/>
+        <img src={backImg} alt="backImg" className='back_img'/>
+      </div>
     </section>
   )
 }
