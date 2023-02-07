759e6265b88577ad05fe2e24a511ef2ef9b23a55
Sebastian <rockmadero@gmail.com>
2023-02-06 20:46:27 -0500

    gallery data creada

---
+++ src/data/galleryData.ts
@@ -0,0 +1,20 @@
+import { imgA1, imgA2, imgA3, imgE1, imgE2, imgE3, imgT1, imgT2, imgT3, imgTS1, imgTS2, imgTS3 } from "../assets/realms";
+
+export const galleryData=[
+    {
+        type:'asphodel',
+        imgs:[imgA1,imgA2,imgA3]
+    },
+    {
+        type: 'elysium',
+        imgs: [imgE1, imgE2, imgE3]
+    },
+    {
+        type: 'tartarus',
+        imgs: [imgT1, imgT2, imgT3]
+    },
+    {
+        type: 'styx',
+        imgs: [imgTS1, imgTS2, imgTS3]
+    },
+]
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,6 @@
 export * from './achieveData';
 export * from './boonsData';
 export * from './characterData';
+export * from './galleryData';
 export * from './realmsData';
 export * from './routesData';
