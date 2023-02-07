a17537ebc7563d4d49749599f8bc763c91afe8a1
Sebastian <rockmadero@gmail.com>
2023-02-06 20:47:31 -0500

    modal creado

--- src/components/GalleryItem.tsx
+++ src/components/GalleryItem.tsx
@@ -1,15 +1,25 @@
-import { galleryData } from "../interfaces"
+import { Realms, realmsName } from "../interfaces"
+import { useContext } from 'react';
+import { UiContext } from "../context";
 
 
-export const GalleryItem = ({ bg, title, details }: galleryData) => {
+export const GalleryItem = (data: Realms) => {
+
+    const { setGalleryType, setShowModal } = useContext(UiContext);
+
+    const handleModal=(type:realmsName)=>{
+        setGalleryType(type);
+        setShowModal(true);
+    }
+
     return (
-        <div className="item" style={{ backgroundImage: `url(${bg})` }}>
+        <div className="item" style={{ backgroundImage: `url(${data.bg})` }}>
             <div className="overlay">
                 <div className="text_container">
-                    <span>{title}</span>
-                    <hr style={{width:'100%'}}/>
-                    <span>{details}</span>
-                    <span className="link">Ver imagenes</span>
+                    <span>{data.title}</span>
+                    <hr style={{ width: '100%' }} />
+                    <span>{data.details}</span>
+                    <span className="link" onClick={()=>handleModal(data.type)}>Ver imagenes</span>
                 </div>
             </div>
         </div>
---
+++ src/components/Modal.tsx
@@ -0,0 +1,63 @@
+import { galleryData } from '../data';
+import { MouseEvent, useContext, useEffect, useState } from 'react';
+import '../styles/modal.css';
+import { UiContext } from '../context';
+
+
+export const Modal = () => {
+
+  const { galleryType, showModal, setShowModal } = useContext(UiContext);
+  const [imgs, setImgs] = useState<string[]>([]);
+  const [indexImg, setIndexImg] = useState<number>(0);
+  useEffect(() => {
+    galleryData.map(data => {
+      (data.type === galleryType) && setImgs(data.imgs);
+    });
+  }, [galleryType, showModal]);
+
+
+  const handlerRight = () => {
+    if (indexImg >= imgs.length - 1) {
+      setIndexImg(0);
+      return;
+    }
+    setIndexImg(indexImg + 1);
+  }
+
+  const handlerLeft = () => {
+    if (indexImg === 0) {
+      setIndexImg(imgs.length - 1);
+      return;
+    }
+    setIndexImg(indexImg - 1);
+  }
+
+  const handleCloseModal = (event: MouseEvent) => {
+    const target = event.target as Element;
+    if (!target.classList.contains('close')) return;
+    setIndexImg(0);
+    setImgs([]);
+    setShowModal(false);
+  }
+
+
+  return (
+    <div className='modal close' onClick={handleCloseModal}>
+      <span className='close' onClick={handleCloseModal}>
+        <svg viewBox="0 0 32 32" width="32" height="32" stroke="currentColor" fill="currentColor"><path className='close' d="M16 2a14 14 0 1 1 0 28 14 14 0 0 1 0-28z m0 12.3L12.7 11.1a1.2 1.2 0 1 0-1.6 1.6L14.3 16 11.1 19.3a1.2 1.2 0 1 0 1.6 1.6L16 17.7 19.3 20.9a1.2 1.2 0 1 0 1.6-1.6L17.7 16 20.9 12.7a1.2 1.2 0 1 0-1.6-1.6L16 14.3z" /></svg>
+      </span>
+      <img src={imgs[indexImg]} alt="imgGalley" />
+
+      <button className="modal_arrow arrow_left" onClick={handlerLeft}>
+
+        <svg viewBox="0 0 32 32" width="32" height="32" stroke="currentColor" fill="currentColor"><path d="M30 4a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v24a2 2 0 0 0 2 2h24a2 2 0 0 0 2-2V4zM0 4a4 4 0 0 1 4-4h24a4 4 0 0 1 4 4v24a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4z m23 11a1 1 0 0 1 0 2H11.4l4.3 4.3a1 1 0 0 1-1.4 1.4l-6-6a1 1 0 0 1 0-1.4l6-6a1 1 0 1 1 1.4 1.4L11.4 15H23z" fillRule="evenodd" /></svg>
+
+      </button>
+
+      <button className="modal_arrow arrow_right" onClick={handlerRight}>
+        <svg viewBox="0 0 32 32" width="32" height="32" stroke="currentColor" fill="currentColor"><path d="M30 4a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v24a2 2 0 0 0 2 2h24a2 2 0 0 0 2-2V4zM0 4a4 4 0 0 1 4-4h24a4 4 0 0 1 4 4v24a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4z m9 11a1 1 0 0 0 0 2h11.6l-4.3 4.3a1 1 0 0 0 1.4 1.4l6-6a1 1 0 0 0 0-1.4l-6-6a1 1 0 1 0-1.4 1.4L20.6 15H9z" fillRule="evenodd" /></svg>
+      </button>
+
+    </div>
+  )
+}
