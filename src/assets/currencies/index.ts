dc2dc30ffba8f0402d4b932eeae2cb9ac42e2199
Sebastian <rockmadero@gmail.com>
2023-02-07 15:00:32 -0500

    currencis creadas

---
+++ src/assets/currencies/index.ts
@@ -0,0 +1,7 @@
+import coin from './coin.png';
+import darkness from './darkness.png';
+import key from './keys.png';
+import nectar from './nectar.png';
+import titanBlood from './titan-blood.png';
+
+export { coin, darkness, key, nectar, titanBlood };
