d82da5f8578ec611a59b9d719f3d960423653ce3
Sebastian <rockmadero@gmail.com>
2023-02-09 18:45:16 -0500

    slider terminado

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
-import { sliderData as data } from "../data";
+import { useEffect } from 'react';
+import { motion as m, Variants } from 'framer-motion';
 import { useSlide } from '../hooks';
+import { sliderData as data } from "../data";
 
 
 
 export const Slider = () => {
 
     const { indexImg, handlerLeft, handlerRight } = useSlide(data.length);
+
     let slideInterval: number;
 
     function autoSlide() {
@@ -18,12 +20,32 @@ export const Slider = () => {
         return () => clearInterval(slideInterval);
     }, [indexImg]);
 
+    const variant: Variants = {
+        initial: {
+            // x: 20,
+            opacity: 0
+        },
+        visible: {
+            // x: 0,
+            opacity: 1
+        },
+        exit: {
+            // x: -20,
+            opacity: 0
+        }
+    }
+
 
     return (
         <div className="slide_show">
-            <img
+            <m.img
+                variants={variant}
                 src={data[indexImg].img}
                 alt={data[indexImg].alt}
+                key={data[indexImg].alt}
+                initial="initial"
+                animate="visible"
+                exit="exit"
             />
 
             <button className="left btn" onClick={handlerLeft}>◀</button>
