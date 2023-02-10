607ed6951fb8df1aee73c6f70f9aee2c6387708e
Sebastian <rockmadero@gmail.com>
2023-02-10 15:47:41 -0500

    footer data creado

---
+++ src/data/footerData.ts
@@ -0,0 +1,39 @@
+import { epicLogo, github, linkedin, playLogo, steam, switchLogo, twitter, xbox } from '../assets';
+
+export const platformsLogos = [
+    {
+        img:steam,
+        title:'Steam'
+    },
+    {
+        img:epicLogo,
+        title:'Epic-games'
+    },
+    {
+        img:playLogo,
+        title:'PlayStation'
+    },
+    {
+        img:xbox,
+        title:'Xbox'
+    },
+    {
+        img:switchLogo,
+        title:'Nintendo switch'
+    }
+];
+
+export const contactLogos = [
+    {
+        img: linkedin,
+        link: '',
+    },
+    {
+        img: twitter,
+        link: 'https://twitter.com/M1999Sebastian'
+    },
+    {
+        img: github,
+        link: 'https://github.com/Sebmm3010'
+    }
+];
--- src/data/index.ts
+++ src/data/index.ts
@@ -2,6 +2,7 @@ export * from './achieveData';
 export * from './boonsData';
 export * from './caracData';
 export * from './characterData';
+export * from './footerData';
 export * from './galleryData';
 export * from './realmsData';
 export * from './routesData';
