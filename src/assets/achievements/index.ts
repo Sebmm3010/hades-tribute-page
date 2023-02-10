a9b7a7fcf62f9d01b718f53527ebc8557aa58f9b
Sebastian <rockmadero@gmail.com>
2023-02-10 15:48:42 -0500

    assest sobre borrados

--- src/assets/achievements/index.ts
+++
@@ -1,5 +0,0 @@
-import euroGoty from './goty_eurogamer.png';
-import ignGoty from './goty_ign.png';
-import tgaGoty from './goty_tga.png';
-
-export {euroGoty, ignGoty, tgaGoty};
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,16 +1,15 @@
 import epicLogo from './epic-logo.png';
 import backImg from './back-img-projectiles.png';
-import github from './github.svg';
+import github from './github.png';
 import goty from './goty.png';
 import heroImg from './hades-hero-img.png';
 import hadesLogo from './hades-logo.png';
-import ignLogo from './ign-logo.png';
-import linkedin from './linkedin.svg';
+import linkedin from './linkedin.png';
 import playLogo from './playstation-logo.png';
 import skull from './skull_hades.png';
 import steam from './steam-logo.png';
 import switchLogo from './switch-logo.png'
-import twitter from './twitter.svg';
+import twitter from './twitter.png';
 import xbox from './xbox-logo.png';
 
-export { epicLogo, backImg, github, goty, heroImg, hadesLogo, ignLogo, linkedin, playLogo, skull, steam, twitter, switchLogo, xbox};
+export { epicLogo, backImg, github, goty, heroImg, hadesLogo, linkedin, playLogo, skull, steam, twitter, switchLogo, xbox};
