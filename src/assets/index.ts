bc847d1017df66b01fd638a6ecd2232a7ab9d727
Sebastian <rockmadero@gmail.com>
2023-02-14 18:57:26 -0500

    logos delete

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,15 +1,10 @@
-import epicLogo from './epic-logo.png';
 import backImg from './back-img-projectiles.png';
 import github from './github.png';
 import goty from './goty.png';
 import heroImg from './hades-hero-img.png';
 import hadesLogo from './hades-logo.png';
 import linkedin from './linkedin.png';
-import playLogo from './playstation-logo.png';
 import skull from './skull_hades.png';
-import steam from './steam-logo.png';
-import switchLogo from './switch-logo.png'
 import twitter from './twitter.png';
-import xbox from './xbox-logo.png';
 
-export { epicLogo, backImg, github, goty, heroImg, hadesLogo, linkedin, playLogo, skull, steam, twitter, switchLogo, xbox};
+export { backImg, github, goty, heroImg, hadesLogo, linkedin, skull, twitter, };
